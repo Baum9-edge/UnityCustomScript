08b4b7d93a1394749a4c0e761d45f57977eb1afd
Sonisam <sonisamara@yahoo.co.jp>
2026-02-23 12:09:30 +0900

    [Add] カスタムテンプレートフ生成関数を追加

---
+++ ScriptTemplate/Assets/BaumCustomScript/ScriptGeneration/ScriptGenerator.cs
@@ -0,0 +1,16 @@
+#nullable enable
+using BaumCustomTemplate.Settings;
+using System.IO;
+using UnityEngine;
+
+namespace BaumCustomTemplate.ScriptGeneration
+{
+    public class ScriptGenerator
+    {
+        public static bool Generate(string templateOsFullPath)
+        {
+            var generateSettings = TemplateSettings.Select();
+
+        }
+    }
+}
